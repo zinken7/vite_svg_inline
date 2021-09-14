ca12e6672ced4f667f30fa9b53453733b990a9ca
Minh Tú Trần <tyrone@Tyrone.local>
2021-09-14 14:45:25 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <SvgIcon name="sun" style="width: 200px" />
+</template>
+<script lang="ts">
+  import { defineComponent } from 'vue'
+  import SvgIcon from "./components/SvgIcon.vue"
+  export default defineComponent({
+    components: {
+      SvgIcon
+    }
+  })
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/SvgIcon.vue
@@ -0,0 +1,20 @@
+<template>
+  <component :is="currentIcon" />
+</template>
+<script lang="ts">
+  import { computed, defineComponent, defineAsyncComponent } from 'vue'
+  export default defineComponent({
+    props: {
+      name: {
+        type: String,
+        required: true
+      },
+    },
+    setup(props) {
+      const currentIcon = computed(() => defineAsyncComponent(() => import(`../icons/${props.name}.svg?component`))).value
+      return {
+        currentIcon
+      }
+    }
+  })
+</script>
